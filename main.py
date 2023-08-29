faaf45c4e3ea4b70fc25510a96c3825a2eaa713c
dougiebudge <dougie.budge@hotmail.co.nz>
2023-08-29 12:58:00 +0100

    Tested and fixed refactored app + cleaned explore

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+from app import Auth, FetchData
+from configparser import ConfigParser
+
+# Pull in my account IDs
+config = ConfigParser()
+config.read("creds.ini")
+client_id = config.get("tokens", "client_id")
+client_secret = config.get("tokens", "client_secret")
+refresh_token = config.get("tokens", "refresh_token")
+
+# Generate my auth token
+auth = Auth(client_id, client_secret, refresh_token)
+token = auth.getToken()
+
+# Get my activity data using the auth token generated
+fetch = FetchData()
+data = fetch.FetchMeData(token, "Activity")
+
+# Save data
+with open("data.json", "w") as f:
+    json.dump(data, f)
